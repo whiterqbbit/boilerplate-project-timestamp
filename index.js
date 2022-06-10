d6c35d2fee43001cd2e037581d9a4c2abc9110ce
whiterqbbit <wedd13@gmail.com>
2022-06-10 13:06:25 +0200

    fix: port

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+let port = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -11,7 +12,7 @@ var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+var listener = app.listen(port, function () {
 	console.log('Your app is listening on port ' + listener.address().port);
 });
 
